0f435a49305991d12b17a240f8a84d6258f81024
Burbon13 <razvanroatis91@gmail.com>
2020-07-09 17:44:30 +0300

    Initialized express app

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const logger = require('./src/utils/logger');
+
+logger.info('Starting server');
+
+logger.info('Initializing Express app');
+const app = express();
+
+const port = 12345;
+logger.info(`Express server listening on port ${port}`);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+logger.info('Initializing Express routes');
+app.get('/test', (req, res) => res.send('Server working! Hello world!'));
+
+logger.info('Server initialization finished successfully!');
